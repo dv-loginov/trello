e837f32d41eab6b694411faf558b96bcabf84447
dv-loginov <dilog@rambler.ru>
2024-05-05 13:33:16 +0300

    добавит компонент logo

---
+++ components/logo.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export const logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+                <Image 
+                    src="/logo.png" 
+                    alt="logo"
+                    height={30}
+                    width={30} 
+                    />
+            </div>
+        </Link>
+    )
+}
