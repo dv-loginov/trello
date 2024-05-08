5ae63963aee4abf3b326067e74bc46992e7ab271
dv-loginov <dilog@rambler.ru>
2024-05-08 12:28:44 +0300

    Добавить prettier и заменить картинку в лого

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link"
 import Image from "next/image"
+import localFont from "next/font/local"
+import { cn } from "@/lib/utils"
 
-export const logo = () => {
+const headingFont = localFont({ src: "../public/fonts/Mulish.woff2" });
+
+export const Logo = () => {
     return (
         <Link href="/">
             <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-                <Image 
-                    src="/logo.png" 
+                <Image
+                    src="/logo.png"
                     alt="logo"
                     height={30}
-                    width={30} 
-                    />
+                    width={30}
+                />
+                <p className={cn("text-lg text-neutral-700", headingFont.className)}>Таскунчик</p>
             </div>
         </Link>
     )
