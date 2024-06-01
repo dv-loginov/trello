ff9abc0998b3682225b023359067cd87c4d22d33
dv-loginov <dilog@rambler.ru>
2024-06-01 12:09:55 +0300

    Добавить навигацию в левой панели

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
-
+import {
+    Activity,
+    CreditCard,
+    Layout,
+    Settings
+} from "lucide-react";
 import { cn } from "@/lib/utils";
-import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 
 export type Organization = {
     id: string;
@@ -26,6 +33,36 @@ export const NavItem = ({
     organization,
     onExpand,
 }: NavItemProps) => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const routes = [
+        {
+            label: "Boards",
+            icon: <Layout className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}`,
+        },
+        {
+            label: "Activity",
+            icon: <Activity className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/activity`,
+        },
+        {
+            label: "Settings",
+            icon: <Settings className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/settings`,
+        },
+        {
+            label: "Billing",
+            icon: <CreditCard className="h-4 w-4 mr-2" />,
+            href: `/organization/${organization.id}/billing`,
+        }
+    ]
+
+    const onClick = (href: string) => {
+        router.push(href)
+    }
+
     return (
         <AccordionItem
             value={organization.id}
@@ -40,18 +77,32 @@ export const NavItem = ({
             >
                 <div className="flex items-center gap-x-2">
                     <div className="w-7 h-7 relative">
-                        <Image 
-                        fill
-                        src={organization.imageUrl} 
-                        alt={`Конторка ${organization.name}`} 
-                        className="rounded-sm object-cover" />
+                        <Image
+                            fill
+                            src={organization.imageUrl}
+                            alt={`Конторка ${organization.name}`}
+                            className="rounded-sm object-cover" />
                     </div>
                     <span className="font-medium text-sm">{organization.name}</span>
                 </div>
             </AccordionTrigger>
-
+            <AccordionContent className="pt-1 text-neytral-700">
+                {routes.map((route) => (
+                    <Button
+                        key={route.href}
+                        size="sm"
+                        onClick={() => onClick(route.href)}
+                        className={cn("w-full font-normal justyfy-start pl-10 mb-1",
+                            pathname===route.href && "bg-sky-500/10 text-sky-700"
+                        )}
+                        variant="ghost"
+                    >
+                        {route.icon}
+                        {route.label}
+                    </Button>
+                ))}
+            </AccordionContent>
         </AccordionItem >
     )
 }
 
-// todo:2:37:52
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -60,7 +60,7 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
         <>
             <div className="font-medium text-xs flex items-center mb-1">
                 <span className="pl-4">
-                    Конторка
+                    Пространство
                 </span>
                 <Button
                     asChild
