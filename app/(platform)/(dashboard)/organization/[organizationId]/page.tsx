e013d783e1f446d8de67c6fb4148694265803618
dv-loginov <dilog@rambler.ru>
2024-06-19 13:41:38 +0300

    Подключение базы данных

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,17 @@
+import { create } from "@/actions/create-board";
 
 const OrganizationIdPage = () => {
-
     return (
         <div className="">
-            Organization Page
+            <form action={create}>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    placeholder="Введите название"
+                    className="border-black border p-1"
+                    required />
+            </form>
         </div>
     );
 }
