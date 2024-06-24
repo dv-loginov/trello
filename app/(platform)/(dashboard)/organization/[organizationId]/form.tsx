3fc8892abe3eab09b4762b58b5eedb1adba7f952
dv-loginov <dilog@rambler.ru>
2024-06-24 15:28:42 +0300

    Добавить компоненты form-input, form-delete, form-button

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,34 +1,21 @@
-"use client"
+'use client';
 
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { useFormState } from "react-dom";
+import { create } from '@/actions/create-board';
+import { Button } from '@/components/ui/button';
+import { useFormState } from 'react-dom';
+import { FormInput } from './form-input';
+import { FormButton } from './form-button';
 
 export const Form = () => {
-    const initialState = { errors: {}, message: "" };
-    const [state, dispatch] = useFormState(create, initialState);
+  const initialState = { errors: {}, message: '' };
+  const [state, dispatch] = useFormState(create, initialState);
 
-    return (
-        <form action={dispatch}>
-            <div className="flex flex-col space-y-2">
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    placeholder="Введите название"
-                    className="border-black border p-1"
-                    required />
-                {state?.errors?.title
-                    ? (
-                        <div>
-                            {state.errors.title.map((error: string) => (
-                                <p key={error} className="text-rose-500">{error}</p>)
-                            )}
-                        </div>
-                    )
-                    : null}
-            </div>
-            <Button type="submit">Отправить</Button>
-        </form>
-    );
+  return (
+    <form action={dispatch}>
+      <div className="flex flex-col space-y-2">
+        <FormInput errors={state?.errors} />
+      </div>
+      <FormButton />
+    </form>
+  );
 };
